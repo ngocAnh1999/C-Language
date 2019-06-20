a0c8897cfd27118d2fedd76370de5564b220eefd
NgocAnh <anhshmily2512@gmail.com>
2019-06-20 11:38:09 +0700

    update

--- start.cpp
+++ start.cpp
@@ -1,14 +1,37 @@
-#include <iostream> 
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <string>
 #include <vector>
 using namespace std;
-int main()
+
+main()
 {
-    cout<<"This is my first C++ Program.";
-    cout<<std::endl<<"and its very easy to learn";
-    int x;
-    cin>>x;
-    cout<<x<<endl;
-    vector<int> vc;
-    vc.push_back(66);
-    cout<<vc[0];
+    string s;
+    cout << "nhập tên: ";
+    cin >> s;
+    cout<< *s.begin()<<endl;
+    cout<<s.append("abcde")<<endl;
+    cout<<s.compare("asss")<<endl;
+    cout<<s.empty()<<endl;
+    cout << "nhập giá trị col, query: \n";
+    int line, query;
+    cin >> line >> query;
+    vector<vector<int>> vt;
+    vt.resize(line);
+    for (int i = 0; i< line; i++){
+        
+        int col;
+        cin >> col;
+        vt[i].resize(col);
+        for (int j=0; j <= col; j++){
+            cin >> vt[i][j];
+        }
+    }
+    for (int i = 0; i < query; i++){
+        int k1, k2;
+        cin >> k1 >> k2;
+        cout << vt[k1][k2]<< "\n";
+    }
+    return 0;
 }
